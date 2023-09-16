3da53c7702308993361c1d67965b90194d348a59
Sai Chebrolu <saivallabha@gmail.com>
2023-09-16 16:49:04 -0400

    Initial Commit

---
+++ Sky/SkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyApp.swift
+//  Sky
+//
+//  Created by Sai Chebrolu on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
