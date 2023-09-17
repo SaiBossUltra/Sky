7ce4e4270d1293609fae1a44b2e3de57e6f105ff
Sai Chebrolu <saivallabha@gmail.com>
2023-09-16 20:56:50 -0400

    Loading View commit

---
+++ Sky/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  Sky
+//
+//  Created by Sai Chebrolu on 9/16/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
